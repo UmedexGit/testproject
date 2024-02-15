831f879dcb8cf4e7a8ee826800a2a470e40cbe36
adarsh2708 <umede@ADARSHSHETTY>
2024-02-15 12:54:57 +0530

    Add project files.

---
+++ testprojectgithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testprojectgithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
